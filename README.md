09d860d972fb3e2d7b5d87747c3a56d1b7e53c9d
Saulo Filho <32524964+saulofilho@users.noreply.github.com>
2020-07-15 16:00:27 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+<h1 align="center">
+    <img alt="Santa Contabilidade" src="https://i0.wp.com/santacontabilidade.com.br/wp-content/uploads/2019/10/Santa-Contabilidade-Digital.jpg" />
+    <br>
+</h1>
+
+<h4 align="center">
+  Página de Materiais.
+</h4>
+
+<p align="center">
+  <a href="#floppy_disk-technologies">Technologies</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#wrench-how-to-use">How To Use</a>
+</p>
+
+## :floppy_disk: Technologies
+
+This project was developed with the following technologies:
+
+-  [Gatsby](https://www.gatsbyjs.org/)
+-  [React](https://reactjs.org/)
+-  [Netlify CMS](https://www.netlifycms.org/)
+-  [Prismjs](https://prismjs.com/)
+-  [react-feather](https://github.com/feathericons/react-feather#readme)
+-  [react-search-field](https://github.com/nutboltu/react-search-field#readme)
+-  [VS Code][vc] with [EditorConfig][vceditconfig] and [ESLint][vceslint]
+
+## :wrench: How To Use
+
+To clone and run this application, you'll need [Git](https://git-scm.com), [Node.js v10.16][nodejs] or higher + [Yarn v1.13][yarn] or higher installed on your computer. From your command line:
+
+```bash
+# Clone this repository
+$ git clone https://github.com/saulofilho/pagina-de-materiais
+
+# Go into the repository
+$ cd pagina-de-materiais
+
+# Install dependencies
+$ yarn install
+
+# Run the app
+$ yarn start
+```
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/71312769-dc26-4abf-8f24-87509844771c/deploy-status)](https://app.netlify.com/sites/pagina-de-materiais/deploys)
+
+[nodejs]: https://nodejs.org/
+[yarn]: https://yarnpkg.com/
+[vc]: https://code.visualstudio.com/
+[vceditconfig]: https://marketplace.visualstudio.com/items?itemName=EditorConfig.EditorConfig
+[vceslint]: https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint
---
+++ gatsby-config.js
@@ -0,0 +1,117 @@
+const postcssPresetEnv = require('postcss-preset-env')
+
+module.exports = {
+  siteMetadata: {
+    title: 'Santa Contabilidade | Blog',
+    siteUrl: 'https://saulofilho.com'
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-yaml',
+    {
+      resolve: 'gatsby-plugin-google-tagmanager',
+      options: {
+        id: 'GTM-123456',
+        includeInDevelopment: false
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-offline',
+      options: {
+        runtimeCaching: [
+          {
+            urlPattern: /(\.js$|\.css$|static\/)/,
+            handler: `cacheFirst`
+          },
+          {
+            urlPattern: /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
+            handler: `staleWhileRevalidate`
+          },
+        ],
+        skipWaiting: true,
+        clientsClaim: true
+      }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'santacontabilidade',
+        short_name: 'santacontabilidade-blog',
+        start_url: '/',
+        background_color: '#00C2BD',
+        theme_color: '#00C2BD',
+        display: 'standalone',
+        icon: `${__dirname}/static/images/logo.svg`
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/static/images`,
+        name: 'images'
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/content`,
+        name: 'pages'
+      }
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          'gatsby-remark-relative-images',
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 800,
+              linkImagesToOriginal: false
+            }
+          },
+          `gatsby-remark-responsive-iframe`
+        ]
+      }
+    },
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        postCssPlugins: [
+          postcssPresetEnv({
+            browsers: '> 0.5%, last 2 versions, ie 11'
+          })
+        ]
+      }
+    },
+    {
+      resolve: `gatsby-plugin-postcss`,
+      options: {
+        postCssPlugins: [
+          require(`postcss-preset-env`)({
+            browsers: '> 0.5%, last 2 versions, ie 11'
+          })
+        ]
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-nprogress',
+      options: {
+        color: 'white',
+        showSpinner: false
+      }
+    },
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+        stylesPath: `${__dirname}/src/cms/admin.css`,
+        enableIdentityWidget: true
+      }
+    },
+    'gatsby-plugin-netlify'
+  ]
+}
